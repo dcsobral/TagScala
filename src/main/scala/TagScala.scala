9cac3178fc2be33b4f41640b49d1022f641f79c7
Daniel C. Sobral <dcsobral@gmail.com>
2012-03-16 12:35:05 -0300

    A few fixes.

--- src/main/scala/TagScala.scala
+++ src/main/scala/TagScala.scala
@@ -2,5 +2,4 @@ package com.github.dcsobral.tagscala
 
 class TagScala {
   def rectify(in: String): String = "<html><body></body></html>"
-
 }
--- src/test/scala/TestTag.scala
+++ src/test/scala/TestTag.scala
@@ -9,10 +9,10 @@ class TestTag extends Specification with matcher.DataTables {
     "Invalid input should be rectified" ^ {
       "Input" || "Output" |
       ""      !! "<html><body></body></html>" |> {
-        (in, out) => checker rectify in must beEqualTo(out)
-      }
-      end
-  }
+        (in: String, out: String) => checker rectify in must beEqualTo(out)
+      } 
+    } ^
+    end
 
   def checker = new TagScala
 }
